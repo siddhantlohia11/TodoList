5039706911d03c316cb9747e00606ad276e34b28
siddhantlohia11 <sidhantlohia11@gmail.com>
2020-03-20 13:26:04 +0530

    With delete functionality and dynamic pages

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
-
+const _ = require('lodash');
 const app = express();
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/newDB', {useNewUrlParser: true, useUnifiedTopology: true});
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = [];
+const defaultlist = [];
 const workItems = [];
 
 const listSchema = new mongoose.Schema ({
@@ -24,8 +24,17 @@ const listSchema = new mongoose.Schema ({
   }
 });
 
+
+const dynamicSchema = new mongoose.Schema ({
+  name: {
+    type: String,
+    required: [true,"Please check you data entry, no name specified!"]
+  },
+  items: [listSchema]
+});
+
 const List = mongoose.model('List', listSchema);
-const Worklist = mongoose.model('Worklist', listSchema);
+const DynamicList = mongoose.model('DynamicList', dynamicSchema);
 
 
 const item1 = new List ({
@@ -37,16 +46,8 @@ const item2 = new List ({
 const item3 = new List ({
       name: "Eat Food"
     });
-const item4 = new Worklist ({
-  name: "Complete all"
-});
-const item5 = new Worklist ({
-  name: "Pending works"
-});
-items.push(item1,item2,item3);
 
-
-
+defaultlist.push(item1,item2,item3);
 
 
 
@@ -69,69 +70,67 @@ app.get("/", function(req, res) {
   });
 });
 
+app.get('/:userid', function(req,res){
+  var dynamic = _.capitalize(req.params.userid);
+  DynamicList.findOne({name: dynamic}, function(err,foundList){
+    if (!err){
+      if (!foundList){
+        const dynamiclist = new DynamicList({
+          name: dynamic,
+          items: defaultlist
+        });
+        dynamiclist.save();
+        res.redirect("/" + dynamic);
+      } else{
+        res.render("list", {listTitle: dynamic, newListItems:foundList.items})
+      }
+    }
+  });
+});
 
 
 app.post("/", function(req, res){
   const item = req.body.newItem;
+  const listName = req.body.list;
 
-    // console.log(req.body.list)
-  if (req.body.list === "Work"){
-    // console.log("ho rahha");
-      const nextItem = new Worklist ({
-          name: item
-        });
-      nextItem.save();
-      // workItems.push(item);
-      res.redirect("/work");
-    } else {
-
-
-      const nextItem = new List ({
-            name: item
-          });
-      nextItem.save();
-      // items.push(item);
-      res.redirect("/");
-    }
-
+  const nextItem = new List ({
+    name: item
   });
+  const day = date.getDate();
+
+  if (listName === day){
+    nextItem.save();
+    res.redirect("/");
+  } else {
+    DynamicList.findOne({name:listName}, function(err,foundList){
+      foundList.items.push(nextItem);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
+});
 
 app.post("/delete", function(req,res){
+  const day = date.getDate();
   var deleteItem = req.body.delete;
-    List.deleteOne({ _id: deleteItem }, function(err){
-    if (err){
-      console.log(err);
-    } else{
-      console.log("Succesfully deleted an item");
-    }
-  });
-  // Worklist.deleteOne({ _id: deleteItem }, function(err){
-  //   if (err){
-  //     console.log(err);
-  //     } else{
-  //     console.log("Succesfully deleted an item");
-  //     }
-  // });
-  res.redirect("/");
-});
-
-app.get("/work", function(req,res){
-
-    Worklist.find({},  function(err,list2){
-      if (list2.length === 0){
-        Worklist.insertMany([item4,item5], function(err){
-            if (err){
-              console.log(err);
-            } else{
-            console.log("Succesfully added all the fruits");
-            }
-        });
-        res.redirect("/work");
-       } else {
-        res.render("list", {listTitle: "Work", newListItems: list2});
+  var namee = req.body.namee;
+  if (namee === day){
+    List.findByIdAndRemove(deleteItem, function(err){
+      if (!err){
+        console.log("Succesfully deleted an item");
+        res.redirect("/");
+        }
+    });
+  } else {
+    DynamicList.findOneAndUpdate({name: namee }, {$pull:{items:{_id: deleteItem}}}, function(err,findList){
+      if (!err){
+        res.redirect("/" + namee);
       }
     });
+  }
 });
+
+
 
 app.get("/about", function(req, res){
   res.render("about");
