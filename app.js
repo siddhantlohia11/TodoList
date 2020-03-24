de139144c2c1c3fcb4303961434959d44c8590a8
siddhantlohia11 <sidhantlohia11@gmail.com>
2020-03-24 23:22:06 +0530

    With login and basic error removed

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
 
