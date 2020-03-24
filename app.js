d6c73e04c84541a031f6821576cc0caa20733b74
siddhantlohia11 <sidhantlohia11@gmail.com>
2020-03-24 18:52:23 +0530

    Yeh complete project h ab yahan se login ka try karne wala hoon

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const date = require(__dirname + "/date.js");
 const _ = require('lodash');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin-Siddhant:qwerty123@cluster0-33zzh.mongodb.net/newDB', {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb+srv://admin-Siddhant:qwerty123@cluster0-33zzh.mongodb.net/newDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect("mongodb://localhost:27017/newDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
 
 app.set('view engine', 'ejs');
